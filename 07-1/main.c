c263fadce2e4dd9693ac19fdd34ac97e3e984410
limye2 <144992260+limye2@users.noreply.github.com>
2023-10-19 15:19:43 +0900

    Initial Commit

---
+++ 07-1/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  07-1
+//
+//  Created by MacBook Air on 2023/10/19.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int flag = 1;
+    int y;
+    while(flag != 0)
+    {
+        y=3;
+        flag = 0;
+    }
+    y=4;
+    return 0;
+}
